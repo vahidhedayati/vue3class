94716e2805704fb714e1aba9398345d20b545b05
VH <badvad@gmail.com>
2020-10-17 14:24:02 +0100

    useMousePosition added to Home.vue

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,16 +2,22 @@
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png" width="30">
     <HelloWorld msg="Hi there"/>
+    {{ x }} {{ y }}
   </div>
 </template>
 
 <script lang="ts">
 import { Options, Vue } from 'vue-class-component'
 import HelloWorld from '@/components/HelloWorld.vue' // @ is an alias to /src
+import { useMousePosition } from '@/composables/useMousePosition'
 @Options({
   components: {
     HelloWorld
   }
 })
-export default class Home extends Vue {}
+export default class Home extends Vue {
+  a = useMousePosition()
+  x = this.a.x
+  y = this.a.y
+}
 </script>
